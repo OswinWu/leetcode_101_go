acbcf561e9c9a6720abbc560881907310057604e
Nothing <22212484+OswinWu@users.noreply.github.com>
2022-08-16 11:48:26 +0800

    [style]

--- Algorithms/455.AssignCookies/assign-cookies.go
+++ Algorithms/455.AssignCookies/assign-cookies.go
@@ -1,4 +1,4 @@
-package assigncookies
+package assignCookies
 
 import "sort"
 
--- Algorithms/455.AssignCookies/assign-cookies_test.go
+++ Algorithms/455.AssignCookies/assign-cookies_test.go
@@ -1,4 +1,4 @@
-package assigncookies
+package assignCookies
 
 import (
 	"testing"
@@ -8,29 +8,29 @@ func Test_findContentChildren(t *testing.T) {
 	testCases := []struct {
 		inputChildren []int
 		inputCookies  []int
-		excepetResult int
+		exceptResult  int
 	}{
 		{
 			inputChildren: []int{1, 2},
 			inputCookies:  []int{1, 2, 3},
-			excepetResult: 2,
+			exceptResult:  2,
 		},
 		{
 			inputChildren: []int{3, 2, 1},
 			inputCookies:  []int{1, 1, 1},
-			excepetResult: 1,
+			exceptResult:  1,
 		},
 		{
 			inputChildren: []int{10, 9, 8, 7},
 			inputCookies:  []int{5, 6, 7, 8},
-			excepetResult: 2,
+			exceptResult:  2,
 		},
 	}
 
 	for _, testCase := range testCases {
 		actual := findContentChildren(testCase.inputChildren, testCase.inputCookies)
-		if actual != testCase.excepetResult {
-			t.Errorf("error result, except %d, got %d, testCase %v", testCase.excepetResult, actual, testCase)
+		if actual != testCase.exceptResult {
+			t.Errorf("error result, except %d, got %d, testCase %v", testCase.exceptResult, actual, testCase)
 		}
 	}
 }
